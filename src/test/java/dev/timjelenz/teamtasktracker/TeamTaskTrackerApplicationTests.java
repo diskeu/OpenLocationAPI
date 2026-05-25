56cdcad5b7060ed65cb500167587aa4146509b24
Tim Jelenz <jelenzt@gmail.com>
2026-05-25 21:44:02 +0200

    inital commit
    - create ground structure of the repo

---
+++ src/test/java/dev/timjelenz/teamtasktracker/TeamTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.timjelenz.teamtasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
