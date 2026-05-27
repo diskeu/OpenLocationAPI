5690ba9d57882f981c3d6e13f5c51813f801e758
Tim Jelenz <jelenzt@gmail.com>
2026-05-27 19:07:06 +0200

    fix: loading `DataSource` failure due no configured `DataSource`
    - fix `DataSourceAutoconfiguration` failure with excluding
      `DataSourceAutoconfiguration.class`.

--- src/main/java/dev/timjelenz/teamtasktracker/TeamTaskTrackerApplication.java
+++ src/main/java/dev/timjelenz/teamtasktracker/TeamTaskTrackerApplication.java
@@ -2,8 +2,13 @@ package dev.timjelenz.teamtasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude={
+		DataSourceAutoConfiguration.class
+	}
+)
 public class TeamTaskTrackerApplication {
 
 	public static void main(String[] args) {
