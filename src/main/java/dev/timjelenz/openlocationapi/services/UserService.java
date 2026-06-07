f8d15019061d29500738d2af33d8b0b62a8f54af
Tim Jelenz <jelenzt@gmail.com>
2026-06-07 18:10:20 +0200

    add: user service `createUser` method
    - initalized the user service and add `createUser` method wich creates
      user per `CreateUserRequest` - DTO

---
+++ src/main/java/dev/timjelenz/openlocationapi/services/UserService.java
@@ -0,0 +1,43 @@
+package dev.timjelenz.openlocationapi.services;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import dev.timjelenz.openlocationapi.dto.requests.CreateUserRequest;
+import dev.timjelenz.openlocationapi.models.User;
+import dev.timjelenz.openlocationapi.repositories.UserRepository;
+
+/**
+ * User Service.
+ */
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    
+    /**
+     * Creates and inserts user into db.
+     * 
+     * @param createUserRequest will be used to create the User
+     */
+    public void createUser(CreateUserRequest createUserRequest) {
+        final String userName = createUserRequest.username();
+
+        if (userRepository.findByUserName(userName).isPresent()) {
+            throw new RuntimeException("User already Exists");
+        }
+        User user = new User(
+            userName,
+            createUserRequest.email(),
+            passwordEncoder.encode(createUserRequest.plainPassword())
+        );
+        userRepository.save(user);
+    }
+}
