e7e8f05efeaf8aac27bfb6fa80efdfbed23ed8ce
Tim Jelenz <jelenzt@gmail.com>
2026-06-10 19:30:56 +0200

    refactor: move `SecurityConfig` into security directory
    - having the `SecurityConfig` extern in a config directory would be
      inconsistent, this commit will fix this

--- src/main/java/dev/timjelenz/openlocationapi/security/SecurityConfig.java
+++ src/main/java/dev/timjelenz/openlocationapi/security/SecurityConfig.java
@@ -1,7 +1,7 @@
-package dev.timjelenz.openlocationapi.configs;
+package dev.timjelenz.openlocationapi.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
