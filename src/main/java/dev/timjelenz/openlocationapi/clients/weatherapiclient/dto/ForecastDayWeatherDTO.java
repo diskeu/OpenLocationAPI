b6aa9a0f0c51659ac79f885a0fd0f52777f6593f
Tim Jelenz <jelenzt@gmail.com>
2026-06-17 19:25:55 +0200

    refactor: Weather specific DTO's to records
    - reduces the boilerplate code

--- src/main/java/dev/timjelenz/openlocationapi/clients/weatherapiclient/dto/ForecastDayWeatherDTO.java
+++ src/main/java/dev/timjelenz/openlocationapi/clients/weatherapiclient/dto/ForecastDayWeatherDTO.java
@@ -4,46 +4,16 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import lombok.Getter;
+public record ForecastDayWeatherDTO(
+   List<ForecastHourWeatherDTO> forecastHourWeather,
 
-@Getter
-public class ForecastDayWeatherDTO {
-
-   // every ForecastDay Object contains a list of forecastHour objects
-   private final List<ForecastHourWeatherDTO> forecastHourWeather;
-
-   private final LocalDateTime date;
-   private final long dateEpoch;
-   private final int code;
-   private final BigDecimal maxTemC;
-   private final BigDecimal minTempC;
-   private final BigDecimal avgTempC;
-   private final BigDecimal maxWindKMH;
-   private final int avgHumidity;
-   private final int uv;
-
-   public ForecastDayWeatherDTO(
-      final List<ForecastHourWeatherDTO> forecastHourWeather,
-
-      final LocalDateTime date,
-      final long dateEpoch,
-      final int code,
-      final BigDecimal maxTemC,
-      final BigDecimal minTempC,
-      final BigDecimal avgTempC,
-      final BigDecimal maxWindKMH,
-      final int avgHumidity,
-      final int uv
-   ) {
-      this.forecastHourWeather = forecastHourWeather;
-      this.date = date;
-      this.dateEpoch = dateEpoch;
-      this.code = code;
-      this.maxTemC = maxTemC;
-      this.minTempC = minTempC;
-      this.avgTempC = avgTempC;
-      this.maxWindKMH = maxWindKMH;
-      this.avgHumidity = avgHumidity;
-      this.uv = uv;
-   }
-}
+   LocalDateTime date,
+   long dateEpoch,
+   int code,
+   BigDecimal maxTemC,
+   BigDecimal minTempC,
+   BigDecimal avgTempC,
+   BigDecimal maxWindKMH,
+   int avgHumidity,
+   int uv
+) {}
--- src/main/java/dev/timjelenz/openlocationapi/clients/weatherapiclient/dto/WeatherResponseDTO.java
+++ src/main/java/dev/timjelenz/openlocationapi/clients/weatherapiclient/dto/WeatherResponseDTO.java
@@ -1,19 +1,6 @@
 package dev.timjelenz.openlocationapi.clients.weatherapiclient.dto;
 
-import lombok.Getter;
-
-@Getter
-public class WeatherResponseDTO<T> {
-
-    private final WeatherLocationDTO weatherLocationDTO;
-    private final T data;
-    
-    public WeatherResponseDTO (
-        final WeatherLocationDTO weatherLocationDTO,
-        final T data
-    ) {
-        this.weatherLocationDTO = weatherLocationDTO;
-        this.data = data;
-    }
-
-}
+public record WeatherResponseDTO<T>(
+    WeatherLocationDTO weatherLocationDTO,
+    T data
+) { }
