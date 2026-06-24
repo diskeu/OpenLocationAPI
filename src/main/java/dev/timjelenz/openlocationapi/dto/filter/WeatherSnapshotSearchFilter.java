376914e93e35d0a542a344bbaa5c7dc3c5a4916b
Tim Jelenz <jelenzt@gmail.com>
2026-06-24 13:12:25 +0200

    add: `WeatherSnapshotSearchFilter`
    - instead of having 14 parameters in the `WeatherSnapshotService` search
      function, the filter reduces the search function parameters length

---
+++ src/main/java/dev/timjelenz/openlocationapi/dto/filter/WeatherSnapshotSearchFilter.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record WeatherSnapshotSearchFilter(
+
+    Integer id,
+
+    LocalDateTime snapshotTimeMin,
+    LocalDateTime snapshotTimeMax,
+
+    LocalDateTime localTimeMin,
+    LocalDateTime localTimeMax,
+
+    BigDecimal temperatureCMin,
+    BigDecimal temperatureCMax,
+
+    BigDecimal windKMHMin,
+    BigDecimal windKMHMax,
+
+    Short humidityMin,
+    Short humidityMax,
+
+    Short uvMin,
+    Short uvMax,
+
+    LocalDateTime sourceLastUpdatedMin,
+    LocalDateTime sourceLastUpdatedMax,
+
+    Short apiConditionalCodeMin,
+    Short apiConditionalCodeMax
+
+) {}
